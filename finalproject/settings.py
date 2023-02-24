67e76d4eb0cc39a627a69fbc2e1eceea87a3272f
luis28ng <lcngtang@gmial.com>
2023-02-24 15:54:24 -0500

    sign up feature added

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'finalproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- finalproject/urls.py
+++ finalproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('solar/', include('solar.urls')),
     path('admin/', admin.site.urls),
-    path("", include("solar.urls"))
+    path("", include("solar.urls")),
 ]
