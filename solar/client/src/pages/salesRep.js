b466e5aebaf159181af6da6f339c3b7fb800950e
Habib ur Rehman <hbieb949@gmail.com>
2023-03-15 20:20:23 -0400

    Experimenting with salesrep tables

--- solar/client/src/pages/salesRep.js
+++ solar/client/src/pages/salesRep.js
@@ -11,7 +11,9 @@ import {
   MDBCol
 } from 'mdb-react-ui-kit';
 import { MDBTable, MDBTableBody, MDBTableHead } from 'mdb-react-ui-kit';
-import Table from 'react-bootstrap/Table'
+import React, { Component } from "react";
+import Table from 'react-bootstrap/Table';
+import { ThemeConsumer } from 'styled-components';
 
 // export default function App() {
 //   const [verticalActive, setVerticalActive] = useState('tab1');
@@ -79,45 +81,74 @@ import Table from 'react-bootstrap/Table'
 // }
 
 
+export default class SalesRep extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            objectData: ""
+        };
+        this.retrieveSalesRepData = this.retrieveSalesRepData.bind(this);
+    }
 
-fetch("https://h0pt17fv6g.execute-api.us-east-1.amazonaws.com/UAT").then((data) => {
-    // console.log(data); DATA IN JSON Format
-    return data.json(); // Converted data to object
-}).then((objectData) => {
-    console.log(objectData);
-});
+    retrieveSalesRepData() {
+        fetch("https://h0pt17fv6g.execute-api.us-east-1.amazonaws.com/UAT")
+        .then((data) => {
+        // console.log(data); DATA IN JSON Format
+        console.log(data);
+        this.state.objectData = data;
+        return data.json(); // Converted data to object
+        }).then((objectData) => {
+            console.log("in the second then");
+            console.log(objectData);
+            this.state.objectData = objectData;
+        }); 
+    }
 
 
-export default function myApp(){
-    return(
-    <div className='container'>
-        <h2 className="text-center">Customer List</h2>
-        <br></br>
-        <div className = "row">
-            <MDBTable className = "table table-striped table-bordered">
-                <MDBTableHead>
-                    <tr>
-                        <th>Customer's Request ID</th>
-                        <th>Customer's First Name</th>
-                        <th>Customer's city</th>
-                    </tr>
-                </MDBTableHead>
-                <MDBTableBody>
-                    {
-                    objectData.map((value) => 
-                    <tr key = {sales_rep}>
-                        <td>{value.request_id}</td>   
-                        <td>{value.first_name}</td>
-                        <td>{value.city}</td>
-                        </tr>
-                            )
-                            };
-                </MDBTableBody>
-            </MDBTable>
-            </div>         
-        </div>
-    )
+    render() {
+        if (this.state.objectData && this.state.objectData.body !== null) {
+            return(
+                <div className='container'>
+                    <h2 className="text-center">Customer List</h2>
+                    <br></br>
+                    <div className = "row">
+                        <MDBTable className = "table table-striped table-bordered">
+                            <MDBTableHead>
+                                <tr>
+                                    <th>Customer's Request ID</th>
+                                    <th>Customer's First Name</th>
+                                    <th>Customer's city</th>
+                                </tr>
+                            </MDBTableHead>
+                            <MDBTableBody>
+                                {
+                                this.state.objectData.body.map((value) => 
+                                <tr key = {value.sales_reps}>
+                                    <td>{value.request_id}</td>   
+                                    <td>{value.first_name}</td>
+                                    <td>{value.city}</td>
+                                    </tr>
+                                        )
+                                }
+                            </MDBTableBody>
+                        </MDBTable>
+                        </div>         
+                    </div>
+            )
+        } else {
+            console.log('calling api now')
+            this.retrieveSalesRepData()
+            return null
+        }
+        
+    }
+
 }
+
+
+// export default function myApp(){
+    
+// }
 
     // this.state = {
     //   customers: []
