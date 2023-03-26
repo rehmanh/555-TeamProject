5f3fe200c3fa4e53182910e998fc188832034452
Harrison Shih <harrisonshih@harrisons-mbp.mynetworksettings.com>
2023-03-25 20:58:20 -0400

    edit test file

--- api/tests/test_setup.py
+++ api/tests/test_setup.py
@@ -1,13 +1,15 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from ..models import User
 
 class TestSetUp(APITestCase):
     def setUp(self):
-        self.login_url = reverse('login')
-        self.user_data ={
-            'email_address': "test@test.com",
-            'password':"Test"
-        }
+        self.login_url = reverse('knox_login')
+        #self.home_url = reverse('home')
+        self.logout_url = reverse('knox_logout')
+        self.user = User.objects.create_user(email_address="habib@habib.com", first_name='habib', last_name='habib', password='test')
+        #self.client.login(email_address="habib@habib.com", password="test")
+        
         return super().setUp()
 
     def tearDown(self):
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,17 +1,20 @@
+from audioop import reverse
 from .test_setup import TestSetUp
+from django.contrib.auth import login as auth_login
 import pdb
 from ..models import User
 class TestViews(TestSetUp):
-    def test_user_cannot_login_with_no_data(self):
-        res = self.client.post(self.login_url)
-        # self.assertEqual(res.status_code, 200)
+    def test_invalid_login(self):
+        res = self.client.post(self.login_url, {'email_address': 'test@test.com', 'password': 'wrongpassword'})
+        self.assertEqual(res.status_code, 400)
         
-    def test_user_can_login(self):
-        res = self.client.post(self.login_url, self.user_data, format = "json")
-        # pdb.set_trace()
-        # email = res.data['email_address']
-        # user = User.object.get(email= email)
-        # user.is_verified = True
-        # user.save()
-        # self.assertEqual(res.status_code, 200)
+    def test_valid_login(self):
+        res = self.client.post(self.login_url, {'email_address': "habib@habib.com", 'password': "test"})
+        self.assertEqual(res.status_code, 200)
+        #self.assertRedirects(res, self.home_url)
+
+    def test_logout(self):
+        self.client.login(email_address="habib@habib.com", password="test")
+        res = self.client.get(self.logout_url)
+        self.assertEqual(res.status_code, 401)
         
