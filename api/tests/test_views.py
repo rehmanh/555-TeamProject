fd3b80bda1433e4c07966ad9dcdf32acc2fe9159
Harrison Shih <harrisonshih@harrisons-mbp.mynetworksettings.com>
2023-03-27 22:03:36 -0400

    test view

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,20 +1,20 @@
-from audioop import reverse
 from .test_setup import TestSetUp
-from django.contrib.auth import login as auth_login
-import pdb
 from ..models import User
+
+
 class TestViews(TestSetUp):
     def test_invalid_login(self):
         res = self.client.post(self.login_url, {'email_address': 'test@test.com', 'password': 'wrongpassword'})
         self.assertEqual(res.status_code, 400)
         
     def test_valid_login(self):
-        res = self.client.post(self.login_url, {'email_address': "habib@habib.com", 'password': "test"})
+        res = self.client.post(self.login_url, {'email_address': 'harry@harry.com', 'password': 'test'})
         self.assertEqual(res.status_code, 200)
-        #self.assertRedirects(res, self.home_url)
 
     def test_logout(self):
-        self.client.login(email_address="habib@habib.com", password="test")
-        res = self.client.get(self.logout_url)
-        self.assertEqual(res.status_code, 401)
+        res = self.client.post(self.login_url, {'email_address': 'harry@harry.com', 'password': 'test'})
+        self.assertEqual(res.status_code, 200)
+        self.client.logout()
+        res = self.client.get('/login')
+        self.assertEqual(res.status_code, 200)
         
