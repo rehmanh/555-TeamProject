0e1f71616e503219dad03cda7f7dac428273a2f1
Harrison Shih <harrisonshih@harrisons-mbp.mynetworksettings.com>
2023-03-27 22:02:36 -0400

    steup

--- api/tests/test_setup.py
+++ api/tests/test_setup.py
@@ -5,10 +5,7 @@ from ..models import User
 class TestSetUp(APITestCase):
     def setUp(self):
         self.login_url = reverse('knox_login')
-        #self.home_url = reverse('home')
-        self.logout_url = reverse('knox_logout')
-        self.user = User.objects.create_user(email_address="habib@habib.com", first_name='habib', last_name='habib', password='test')
-        #self.client.login(email_address="habib@habib.com", password="test")
+        self.user = User.objects.create_user(email_address="harry@harry.com", first_name='harry', last_name='shih', password='test')
         
         return super().setUp()
 
