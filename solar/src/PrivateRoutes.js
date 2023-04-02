2b5ed0e63c8fbb92b09d36d63a35f85bd6b4b6e5
Harrison Shih <harrisonshih@harrisons-mbp.mynetworksettings.com>
2023-04-02 15:57:07 -0400

    private route

--- solar/src/PrivateRoutes.js
+++ solar/src/PrivateRoutes.js
@@ -1,13 +1,19 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import React, { Component, useEffect, useState } from "react";
 
+// const [isLoggedIn] = useState(localStorage !== null
+//     && localStorage.getItem('token') !== null
+//     && localStorage.getItem('roleId') !== null
+//     && localStorage.getItem('userId') !== null);
+// const [roleId] = useState(localStorage.getItem('roleId'));
+
 const SaRoute = () => {
     const [isLoggedIn] = useState(localStorage !== null
         && localStorage.getItem('token') !== null
         && localStorage.getItem('roleId') !== null
         && localStorage.getItem('userId') !== null);
     const [roleId] = useState(localStorage.getItem('roleId'));
-    
+
     return (
         isLoggedIn && (roleId == 1 || roleId == 2) ? <Outlet /> : <Navigate to='/login' />
     )
@@ -19,10 +25,10 @@ const OpRoute = () => {
         && localStorage.getItem('roleId') !== null
         && localStorage.getItem('userId') !== null);
     const [roleId] = useState(localStorage.getItem('roleId'));
-    
+
     return (
         isLoggedIn && (roleId == 1 || roleId == 3) ? <Outlet /> : <Navigate to='/login' />
     )
 }
 
-export {SaRoute, OpRoute,}
+export { SaRoute, OpRoute, }
