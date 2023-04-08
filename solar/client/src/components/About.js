6b28dcf31a06edb183b1319a273cccd33ab8f91b
Corny Mac <kevin130366@gmail.com>
2023-04-08 17:02:41 -0400

    webpack config to fix image issues

--- solar/client/src/components/About.js
+++ solar/client/src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import imagePath from '../img/Solar1.jpg';
 function About() {
+    
     return (
-        <div>About</div>
+<div>
+    <h1>Image Display</h1>
+    <div id="imageContainer">
+        <img src={imagePath} alt="Image" width="300" height="200" />
+    </div>
+</div>
     )
 }
 
 export default About
+
