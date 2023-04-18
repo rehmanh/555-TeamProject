899591382f9d2b3ce2f31d8674ad72481956962e
chandravamsireddy <chandravamsi169@gmail.com>
2023-04-18 09:17:11 -0400

    ..

--- solar/client/src/components/AnimatedRoutes.js
+++ solar/client/src/components/AnimatedRoutes.js
@@ -13,10 +13,8 @@ import OpManager from '../pages/OpManager';
 import CoCheck from '../pages/coCheck';
 import ConstructionManager from '../pages/ConstructionManager';
 import { AnimatePresence } from 'framer-motion';
-import Payment from '../pages/payment';
 import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-const stripePromise = loadStripe('your_stripe_public_key');
+
 function AnimatedRoutes() {
     const location = useLocation();
     return (
@@ -39,8 +37,6 @@ function AnimatedRoutes() {
                 <Route exact path='/userprog' element={<Userprog />} />
                 <Route exact path='/oper' element={<Operation />} />
                 <Route exact path= '/coCheck' element={<CoCheck />}/>
-                <Route exact path= '/payments' element={<Elements stripe={stripePromise}><Payment /></Elements>}/>
-
                 {/* <Route exact path='/nav' element={<Navbar />}/> */}
             </Routes>
         </AnimatePresence>
