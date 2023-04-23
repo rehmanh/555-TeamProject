efcebe669dcf6ba2fa5a8b7908b4fb0d497a6c03
Djayanth15 <dannana.jayanth@gmail.com>
2023-04-23 15:00:21 -0400

    few changes to tracking page

--- solar/client/src/pages/TrackingStages.js
+++ solar/client/src/pages/TrackingStages.js
@@ -6,15 +6,29 @@ export const TrackingStages = (props) => {
   const [formStepsNum, setFormStepsNum] = useState(0);
 
   useEffect(() => {
-    if (props.Tstatus === "Stage 1") {
+    if (props.Tstatus === "INITIATED") {
       setFormStepsNum(0);
-    } else if (props.Tstatus === "Stage 2") {
+    } else if (props.Tstatus === "SALE-OPS") {
       setFormStepsNum(1);
+    } else if (props.Tstatus === "OPS-CONST") {
+      setFormStepsNum(2);
+    } else if (props.Tstatus === "CONST-SITE") {
+      setFormStepsNum(3);
+    } else if (props.Tstatus === "SITE-CONST") {
+      setFormStepsNum(4);
+    } else if (props.Tstatus === "EST-DONE") {
+      setFormStepsNum(5);
+    } else if (props.Tstatus === "PAYMENT-DONE") {
+      setFormStepsNum(6);
+    } else if (props.Tstatus === "CUST-SCHEDULED") {
+      setFormStepsNum(7);
+    } else if (props.Tstatus === "DONE") {
+      setFormStepsNum(8);
     }
   }, [props.Tstatus]);
 
   // Make sure that the formStepsNum variable does not go out of bounds
-  const normalizedFormStepsNum = Math.min(Math.max(formStepsNum, 0), 3);
+  const normalizedFormStepsNum = Math.min(Math.max(formStepsNum, 0), 8);
 
   return (
     <div className="form">
@@ -33,34 +47,77 @@ export const TrackingStages = (props) => {
         <div
           className="progress"
           id="progress"
-          style={{ width: `${(normalizedFormStepsNum / 3) * 100}%` }}
+          style={{ width: `${(normalizedFormStepsNum / 8) * 100}%` }}
         ></div>
 
         <div
           className={`progress-step ${
-            normalizedFormStepsNum === 0 && "progress-step-active" || normalizedFormStepsNum === 1 && "progress-step-active"|| normalizedFormStepsNum === 2 && "progress-step-active"|| normalizedFormStepsNum === 3 && "progress-step-active"
+            normalizedFormStepsNum === 0 && "progress-step-active" || normalizedFormStepsNum === 1 && "progress-step-active"|| normalizedFormStepsNum === 2 && "progress-step-active"|| normalizedFormStepsNum === 3 && "progress-step-active"|| normalizedFormStepsNum === 4 && "progress-step-active"|| normalizedFormStepsNum === 5 && "progress-step-active"|| normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
           }`}
           data-title="Stage 1"
-        ></div>
+        > <p>{props.Tstatus === "INITIATED" ? "Order initiated" : ""}</p></div>
         <div
           className={`progress-step ${
-            normalizedFormStepsNum === 1 && "progress-step-active" || normalizedFormStepsNum === 2 && "progress-step-active"|| normalizedFormStepsNum === 3 && "progress-step-active"
+            normalizedFormStepsNum === 1 && "progress-step-active" || normalizedFormStepsNum === 2 && "progress-step-active"|| normalizedFormStepsNum === 3 && "progress-step-active"|| normalizedFormStepsNum === 4 && "progress-step-active"|| normalizedFormStepsNum === 5 && "progress-step-active"|| normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
           }`}
           data-title="Stage 2"
         ></div>
         <div
           className={`progress-step ${
-            normalizedFormStepsNum === 2 && "progress-step-active" || normalizedFormStepsNum === 3 && "progress-step-active"
+            normalizedFormStepsNum === 2 && "progress-step-active" || normalizedFormStepsNum === 3 && "progress-step-active"|| normalizedFormStepsNum === 4 && "progress-step-active"|| normalizedFormStepsNum === 5 && "progress-step-active"|| normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
           }`}
           data-title="Stage 3"
         ></div>
         <div
           className={`progress-step ${
-            normalizedFormStepsNum === 3 && "progress-step-active"
+            normalizedFormStepsNum === 3 && "progress-step-active"|| normalizedFormStepsNum === 4 && "progress-step-active"|| normalizedFormStepsNum === 5 && "progress-step-active"|| normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
           }`}
           data-title="Stage 4"
         ></div>
+        <div
+          className={`progress-step ${
+            normalizedFormStepsNum === 4 && "progress-step-active"|| normalizedFormStepsNum === 5 && "progress-step-active"|| normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
+          }`}
+          data-title="Stage 5"
+        ></div>
+        <div
+          className={`progress-step ${
+            normalizedFormStepsNum === 5 && "progress-step-active"|| normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
+          }`}
+          data-title="Stage 6"
+        ></div>
+        <div
+          className={`progress-step ${
+            normalizedFormStepsNum === 6 && "progress-step-active"|| normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
+          }`}
+          data-title="Stage 7"
+        ></div>
+        <div
+          className={`progress-step ${
+            normalizedFormStepsNum === 7 && "progress-step-active"|| normalizedFormStepsNum === 8 && "progress-step-active"
+          }`}
+          data-title="Stage 8"
+        ></div>
+        <div
+          className={`progress-step ${
+            normalizedFormStepsNum === 8 && "progress-step-active"
+          }`}
+          data-title="Stage 9"
+        ></div>
       </div>
+      <div>
+      <h1>Status: 
+      <p>{formStepsNum === 0 ? "New request" : ""}</p>
+      <p>{props.Tstatus === "SALE-OPS" ? "Sales rep assigned" : ""}</p>
+      <p>{props.Tstatus === "OPS-CONST" ? "Construction manager assigned" : ""}</p>
+      <p>{props.Tstatus === "CONST-SITE" ? "Site surveyor assigned" : ""}</p>
+      <p>{props.Tstatus === "SITE-CONST" ? "Site survey finished" : ""}</p>
+      <p>{props.Tstatus === "EST-DONE" ? "Estimation finished" : ""}</p>
+      <p>{props.Tstatus === "PAYMENT-DONE" ? "Payment done" : ""}</p>
+      <p>{props.Tstatus === "CUST-SCHEDULED" ? "Project deployment scheduled" : ""}</p>
+      <p>{props.Tstatus === "DONE" ? "Project deployed" : ""}</p>
+      </h1>
+    </div>
     </div>
   );
 };
